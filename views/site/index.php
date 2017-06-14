377ead8ef6f270b3bdc0f3ef8d5a299d1902bcca
Alexey Dench <dench@i.ua>
2017-06-14 23:55:59 +0300

    Update

--- views/site/index.php
+++ views/site/index.php
@@ -10,30 +10,18 @@ use yii\helpers\Url;
 <div class="container">
     <!--<h1><?= $this->params['page']->h1 ?></h1>-->
 
-    <div class="row">
-        <?php foreach ($items as $item) : ?>
+    <div class="row cards">
+        <?php foreach ($items as $child) : ?>
             <div class="col-xs-6 col-md-4 card block-link">
                 <div class="card-content">
-                    <h3><a href="<?= Url::to(['portfolio/view', 'slug' => $item->slug]) ?>" class="card-photo"><?= $item->name ?></a></h3>
+                    <h3><a href="<?= \yii\helpers\Url::to(['view', 'slug' => $child->slug]) ?>" class="card-photo"><?= $child->name ?></a></h3>
                 </div>
-                <img src="<?= $item->cover ?>" alt="" class="img-responsive">
+                <?php if ($image = current($child->images)) : ?>
+                    <img src="<?= ImageHelper::thumb($image->id, 'cover') ?>" alt="<?= $image->alt ?>" class="img-responsive">
+                <?php endif; ?>
             </div>
         <?php endforeach; ?>
     </div>
 
     <?= $this->params['page']->text ?>
-
-    <?php
-
-    echo ImageHelper::thumb(5,'small');
-
-    echo "<br>";
-
-    echo ImageHelper::thumb(5,'small');
-
-    echo "<br>";
-
-    echo \yii\helpers\StringHelper::basename('dench.jpg', 'jpg');
-
-    ?>
 </div>
