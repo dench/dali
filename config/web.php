74aef4d9118626b92dc4edea845541496c286a86
Alexey Dench <dench@i.ua>
2017-07-01 21:19:15 +0300

    Assets min

--- config/web.php
+++ config/web.php
@@ -38,6 +38,26 @@ $config = [
                 'blog/<slug:[0-9a-z\-]+>' => 'blog/view',
             ],
         ],
+        'assetManager' => [
+            'class' => 'yii\web\AssetManager',
+            'bundles' => [
+                'yii\web\JqueryAsset' => [
+                    'js' => [
+                        YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
+                    ]
+                ],
+                'yii\bootstrap\BootstrapAsset' => [
+                    'css' => [
+                        YII_ENV_DEV ? 'css/bootstrap.css' : 'css/bootstrap.min.css',
+                    ]
+                ],
+                'yii\bootstrap\BootstrapPluginAsset' => [
+                    'js' => [
+                        YII_ENV_DEV ? 'js/bootstrap.js' : 'js/bootstrap.min.js',
+                    ]
+                ]
+            ],
+        ],
     ],
 ];
 
