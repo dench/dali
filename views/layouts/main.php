bc6503f4ef10d8b9d0ef9c4af89f6970abdd2a17
Alexey Dench <dench@i.ua>
2017-07-30 13:21:34 +0300

    Update

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -18,6 +18,7 @@ SiteAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
