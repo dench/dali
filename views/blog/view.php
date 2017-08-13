d9e2badf59fa460b11b1077ac33b91a8c68716c2
Alexey Dench <dench@i.ua>
2017-08-13 11:35:10 +0300

    Update

--- views/blog/view.php
+++ views/blog/view.php
@@ -2,7 +2,7 @@
 /** @var $this yii\web\View */
 /** @var $model app\models\Page */
 ?>
-<div class="container">
+<div class="container blog">
     <h1 class="title"><?= $this->params['page']->h1 ?></h1>
 
     <?= $this->params['page']->text ?>
