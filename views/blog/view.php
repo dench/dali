2c6744cc4f82009d4a101c0ee799634a2f1d0de0
Alexey Dench <dench@i.ua>
2017-09-24 17:11:18 +0300

    Add breadcrumbs

--- views/blog/view.php
+++ views/blog/view.php
@@ -1,9 +1,15 @@
 <?php
 /** @var $this yii\web\View */
 /** @var $model app\models\Page */
+
+$this->params['breadcrumbs'][] = [
+    'label' => 'Блог',
+    'url' => ['blog/index'],
+];
+$this->params['breadcrumbs'][] = '';
 ?>
 <div class="container blog">
-    <h1 class="title"><?= $this->params['page']->h1 ?></h1>
+    <h1><?= $this->params['page']->h1 ?></h1>
 
     <?= $this->params['page']->text ?>
 </div>
