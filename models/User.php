3ec07af0396facec55ea28ad9b3d3ab45ce072a3
Alexey Dench <dench@i.ua>
2017-10-09 11:21:24 +0300

    Update User.php

--- models/User.php
+++ models/User.php
@@ -11,13 +11,6 @@ class User extends \yii\base\Object implements \yii\web\IdentityInterface
     public $accessToken;
 
     private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
         '101' => [
             'id' => '101',
             'username' => 'demo',
