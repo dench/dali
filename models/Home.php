a2080b48bb1e182345cf82cdb6f87eafb4ed1e5a
Alexey Dench <dench@i.ua>
2018-02-10 22:26:09 +0200

    Update

--- models/Home.php
+++ models/Home.php
@@ -120,7 +120,7 @@ class Home extends ActiveRecord
     {
         if (empty($this->link)) {
             if (in_array(2, $this->page->parent_ids)) {
-                return Url::to(['portfolio/view', 'slug' => $this->page->slug]);
+                return Url::to(['portfolio/category', 'slug' => $this->page->slug]);
             } else {
                 return Url::to(['services/view', 'slug' => $this->page->slug]);
             }
