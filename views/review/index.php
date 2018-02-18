24ee8021ed4a1d78cef191f6e057a9e250cc30ad
Alexey Dench <dench@i.ua>
2018-02-18 13:50:38 +0200

    Update

--- views/review/index.php
+++ views/review/index.php
@@ -2,7 +2,7 @@
 /* @var $this yii\web\View */
 /* @var $page \dench\page\models\Page */
 ?>
-<<div class="container">
+<div class="container">
     <h1 class="title"><?= $page->h1 ?></h1>
     <?= $page->text ?>
 </div>
